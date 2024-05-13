5cdb04a1b525ac77d81c3664c462a28221399c3e
Pallabi <pallabilama7@gmail.com>
2024-05-13 20:23:27 +0545

    Add project files.

---
+++ Models/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace FirstProject.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string? Title { get; set; }
+
+        public bool? Status { get; set; }
+    }
+}
